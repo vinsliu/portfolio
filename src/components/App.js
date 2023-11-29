699b42694a8f2d8cd8df3bd00efddb0e2c126c0f
Vincent Liu <Vincent00lav@outlook.com>
2023-11-30 00:02:41 +0100

    Configure React app for deployment to GitHub Pages

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from '../assets/logo.svg';
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>text blabla</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
